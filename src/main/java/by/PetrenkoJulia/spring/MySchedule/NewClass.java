11a8eec704453dcffd3b2f474efac081517be036
Yuliya Piatrenka <JuliaVPetrenko@gmail.com>
2021-02-02 19:36:34 +0300

    Adding class NewClass.java

---
+++ src/main/java/by/PetrenkoJulia/spring/MySchedule/NewClass.java
@@ -0,0 +1,13 @@
+package by.PetrenkoJulia.spring.MySchedule;
+
+public class NewClass {
+    private String s;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
