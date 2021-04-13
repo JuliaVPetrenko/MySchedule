06c6a786fb9ad0b61705516425a13f010cc911ff
Yuliya Piatrenka <JuliaVPetrenko@gmail.com>
2021-04-13 20:59:54 +0300

    fixed liquibase:rollback

--- src/main/resources/db/changelog/v-2.0/01-delete-data-into-table.sql
+++ src/main/resources/db/changelog/v-2.0/01-delete-data-into-table.sql
@@ -6,23 +6,23 @@ GO
 
 delete users_roles.* from users_roles
   join users on users_roles.user_id = users.id
-where username in ('Liz','Nick')
+where username in ('Admin', 'Liz','Nick')
 
 GO
 
-delete users
+delete from users
 where username in ('Admin','Liz','Nick')
 
 GO
 
 delete users_tasks.* from users_tasks
    join tasks on users_tasks.task_id = tasks.id
-where taskname in ('task1', 'task2')
+where taskname in ('task1', 'task2', 'task3')
 
 GO
 
 delete from tasks
-where taskname in ('task1', 'task2')
+where taskname in ('task1', 'task2', 'task3')
 
 GO
 
